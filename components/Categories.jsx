6d7556491494e7b876ab15d47aa317ee9c5ddbbb
Gary <gkozlowskibusiness@outlook.com>
2023-08-11 01:25:31 -0400

    first commit

---
+++ components/Categories.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+
+import { getCategories } from '../services';
+
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
+  return (
+    <div className="bg-gradient-to-tl from-black to-gray-900 border border-gray-700  shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl text-white mb-8 font-semibold border-b pb-4">Categories</h3>
+      {categories.map((category, index) => (
+        <Link key={index} href={`/category/${category.slug}`}>
+          <span className={`cursor-pointer text-white block ${(index === categories.length - 1) } pb-3 mb-3`}>{category.name}</span>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import React, { useEffect, useState } from 'react';
+import { Layout } from '../components'
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+
+  <Layout>
+  <Component {...pageProps} />
+  </Layout>
+  
+  )
 }

90f117952ae089cd7eb553afb8f0c838d60f0622
Gary <gkozlowskibusiness@outlook.com>
2023-08-15 13:55:28 -0400

    commit 12

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
 import About  from '../components/Portfolio/About'
@@ -6,12 +5,9 @@ import About  from '../components/Portfolio/About'
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8 ">
-      {/*<FeaturedPosts/>*/}
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-    
         <div className="lg:col-span-8 col-span-1">
         <div><About /></div>
-
           {posts.map((post, index) => (
             <PostCard key={index} post={post.node} />
           ))}

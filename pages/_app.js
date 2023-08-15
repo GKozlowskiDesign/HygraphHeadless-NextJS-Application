b2e13b8cf060a0f0cfe9fad18bc6b204a1fc277b
Gary <gkozlowskibusiness@outlook.com>
2023-08-15 11:07:41 -0400

    commit 10

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Layout } from '../components'
 import '@/styles/globals.scss'
+import { Analytics } from '@vercel/analytics'
 
 export default function App({ Component, pageProps }) {
   return (
 
   <Layout>
   <Component {...pageProps} />
+  <Analytics />
   </Layout>
   
   )

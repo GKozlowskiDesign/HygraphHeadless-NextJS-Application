4dcac8056b610effd87f60c3c92b28df32c00503
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-19 21:10:15 +0000

    commit ten

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,96 +1,72 @@
-import React from 'react';
-import moment from 'moment';
+import React from 'react'
+
+import moment from 'moment'
+import Image from 'next/image'
 import { RichText } from '@graphcms/rich-text-react-renderer'
 
-const PostDetail = ({ post }) => {
-  const getContentFragment = (index, text, obj, type) => {
-    let modifiedText = text;
-
-    if (obj) {
-      if (obj.bold) {
-        modifiedText = (<b key={index}>{text}</b>);
-      }
-
-     
-      if (obj.italic) {
-        modifiedText = (<em key={index}>{text}</em>);
-      }
-
-     
-      if (obj.underline) {
-        modifiedText = (<u key={index}>{text}</u>);
-      }
-
-
-    switch (type) {
-      case 'heading-three':
-        return <h3 key={index} 
-                 className="text-4xl font-semibold mb-4 text-black">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h3>;
-     
-      case 'paragraph':
-        return <p key={index} 
-                 className="mb-8 text-black">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</p>;
-     
-      case 'heading-four':
-        return <h4 key={index} 
-                 className="text-xl font-semibold mb-4 text-gray-600">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
-
-        case 'heading-six':
-        return <h6 key={index} 
-                 className="text-lg font-semibold mb-4 text-gray-600">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h6>;
-
-        
-      case 'image':
-        return (
-          <img key={index}
-            alt={obj.title}
-            height={obj.height}
-            width={obj.width}
-            src={obj.src}
-            className='rounded-lg border-2 border-black shadow-lg'
-          /> 
-        );
-      default:
-        return modifiedText;
-    }
-  };
-
-  return (
-    <>
-      <div className="bg-white border border-gray-400 shadow-xl rounded-lg lg:p-8 pb-12 mb-8">
-        <div className="relative overflow-hidden shadow-md mb-6">
-          <img src={post.featuredImage.url} alt="" className="object-top h-full w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg" />
+const PostDetail = ({ post }) => (
+  <div className="mb-8 rounded-lg bg-white pb-12 shadow-lg lg:p-8">
+    <div className="relative mb-6 overflow-hidden shadow-md">
+      <Image
+        src={post.featuredImage.url}
+        alt=""
+        className="h-full w-full rounded-t-lg object-cover  object-top shadow-lg lg:rounded-lg"
+      />
+    </div>
+    <div className="px-4 lg:px-0">
+      <div className="mb-8 flex w-full items-center">
+        <div className="mr-8 hidden items-center justify-center md:flex lg:mb-0 lg:w-auto">
+          <img
+            alt={post.author.name}
+            height="30px"
+            width="30px"
+            className="rounded-full align-middle"
+            src={post.author.photo.url}
+          />
+          <p className="ml-2 inline align-middle text-lg font-medium text-gray-700">
+            {post.author.name}
+          </p>
         </div>
-        <div className="px-4 lg:px-0">
-          <div className="flex items-center mb-8 w-full"> 
-            <div className="hidden md:flex justify-center lg:mb-0 lg:w-auto mr-8 items-center">
-              <img
-                alt={post.author.name}
-                height="30px"
-                width="30px"
-                className="align-middle rounded-full"
-                src={post.author.photo.url}
-              />
-              <p className="inline align-middle text-gray-700 ml-2 font-medium text-lg">{post.author.name}</p>
-            </div>
-            <div className="font-medium text-gray-700">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-              </svg>
-              <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
-            </div>
-          </div>
-          <h1 className="mb-8 text-3xl text-black font-semibold">{post.title}</h1>
-          <RichText content={post.content.raw.children.map((typeObj, index) => {
-            const children = typeObj.children.map((item, itemindex) => getContentFragment(itemindex, item.text, item));
-
-            return getContentFragment(index, children, typeObj, typeObj.type);
-          })} />
+        <div className="font-medium text-gray-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mr-2 inline h-6 w-6 text-pink-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+            />
+          </svg>
+          <span className="align-middle">
+            {moment(post.createdAt).format('MMM DD, YYYY')}
+          </span>
         </div>
       </div>
+      <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+      <RichText content={post.content.raw} 
+                renderers={{
+                      h1: ({ children }) => <h1 className="text-white">{children}</h1>,
+                      h2: ({ children }) => <h2 className="text-white">{children}</h2>,
+                      h3: ({ children }) => <h3 className="text-white">{children}</h3>,
+                      h4: ({ children }) => <h4 className="text-white">{children}</h4>,
+                      h5: ({ children }) => <h5 className="text-white">{children}</h5>,
+                      h6: ({ children }) => <h6 className="text-white">{children}</h6>,
+                      p: ({ children }) => <p className="text-white">{children}</p>,
+                      a: ({ children }) => <a className="text-white">{children}</a>,
+                      bold: ({ children }) => <bold className="text-white">{children}</bold>,
+                      underline: ({ children }) => <underline className="text-white">{children}</underline>,
+                      img: ({ src }) => <img className="text-white">{src}</img>
+            
+                }}/>
 
-    </>
-  );
-};
+    </div>
+  </div>
+)
 
-export default PostDetail;
+export default PostDetail
+

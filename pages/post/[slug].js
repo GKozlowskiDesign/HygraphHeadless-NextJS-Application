ba0b066b4020bb029cd09d88357f83ac3c32d5b7
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-19 21:09:50 -0400

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -18,8 +18,8 @@ const PostDetails = ({ post }) => {
             <div className="col-span-1 lg:col-span-8">
                 <PostDetail post={post} />
                 <Author author={post.author} />
-                <CommentsForm slug={post.slug} />
-                <Comments slug={post.slug} />
+             /*   <CommentsForm slug={post.slug} /> */
+             /*   <Comments slug={post.slug} />     */
             </div>
             <div className="col-span-1 lg:col-span-4">
             <div className="relative lg:sticky top-8">

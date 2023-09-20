de4e32bf0e78e6d2c784c365f3ebfe95a0f628d0
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-20 10:30:01 -0400

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,17 +1,12 @@
 import React from "react";
 import { useRouter } from "next/router";
- 
 import { getPosts, getPostDetails } from '../../services';
-
 import { PostDetail, Categories, PostWidget, Loader } from '../../components'
-
 const PostDetails = ({ post }) => {
   const router = useRouter();
-
   if(router.isFallback) {
     return <Loader/>
   }
-  
   return (
     <div className="container mx-auto px-10 mb-8">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
@@ -30,15 +25,12 @@ const PostDetails = ({ post }) => {
 }
 
 export default PostDetails
-
 export async function getStaticProps({ params }) { 
     const data = await getPostDetails(params.slug);
-
     return {
       props: { post: data }
     }
   }
-
   export async function getStaticPaths() {
     const posts = await getPosts()
 

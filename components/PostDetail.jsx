1a832c0470efeab1582e0fe024de0b8c23990619
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-20 10:46:42 -0400

    Update PostDetail.jsx

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -50,7 +50,7 @@ const PostDetail = ({ post }) => (
                 renderers={{
                       h1: ({ children }) => <h1 className="text-black font-bold text-3xl">{children}</h1>,
                       h2: ({ children }) => <h2 className="text-black font-bold text-2xl mt-4">{children}</h2>,
-                      h3: ({ children }) => <h3 className="text-blue-800 font-bold text-2xl underline mb-6 hover:border-sky-500">{children}</h3>,
+                      h3: ({ children }) => <h3 className="text-blue-800 font-bold text-2xl underline mb-6 hover:text-sky-500">{children}</h3>,
                       h4: ({ children }) => <h4 className="text-stone-700 text-base font-medium mt-2">{children}</h4>,
                       h5: ({ children }) => <h5 className="text-black text-base">{children}</h5>,
                       h6: ({ children }) => <h6 className="text-black">{children}</h6>,

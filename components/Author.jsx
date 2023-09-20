7319ff09b670f63184693ae1f9db4e6fa620f8b8
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-19 21:22:37 -0400

    Update Author.jsx
    Updating the color schema of the author component section at the bottom of each post.

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import Image from 'next/image';
-
 const Author = ({ author }) => {
   return (
-        <div className="left-0 mt-20 mb-8 p-12 relative rounded-lg bg-gradient-to-tl from-black to-gray-900 border border-gray-700">
+        <div className="left-0 mt-20 mb-8 p-12 relative rounded-lg bg-white shadow-lg border border-gray-700">
             <div className="absolute left-0 right-0 -top-14">
                 <Image
                 alt={author.name}
                 unoptimized
                 height='100'
                 width='100'
-                className="align-middle rounded-full"
+                className="align-middle rounded-full shadow-lg"
                 src={author.photo.url}
                 />
             </div>
-
-            <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
-            <p className="text-white text-lg">{author.bio}</p>
-           
+            <h3 className="text-black my-4 text-xl font-bold">{author.name}</h3>
+            <p className="text-black text-lg">{author.bio}</p>
         </div>
-
     )
 };
 

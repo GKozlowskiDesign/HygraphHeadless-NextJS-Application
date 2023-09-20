a3757447af941cb22f12b42ad90ffc94d0d6d987
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-20 13:57:37 -0400

    Update PostDetail.jsx

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -49,7 +49,7 @@ const PostDetail = ({ post }) => (
       <RichText content={post.content.raw} 
                 renderers={{
                       h1: ({ children }) => <h1 className="text-black font-bold text-3xl">{children}</h1>,
-                      h2: ({ children }) => <h2 className="text-black font-bold text-2xl mt-4">{children}</h2>,
+                      h2: ({ children }) => <h2 className="text-black font-bold text-2xl mt-4 mb-2">{children}</h2>,
                       h3: ({ children }) => <h3 className="text-blue-800 font-bold text-2xl underline mb-6 hover:text-sky-500">{children}</h3>,
                       h4: ({ children }) => <h4 className="text-stone-700 text-base font-medium mt-2">{children}</h4>,
                       h5: ({ children }) => <h5 className="text-black text-base">{children}</h5>,
@@ -64,7 +64,6 @@ const PostDetail = ({ post }) => (
                     code_block: ({ children }) => <code className="mx-24 mb-8 text-sm sm:text-sm inline-flex text-left items-center space-x-4 bg-gray-700 text-gray-200 rounded-md p-8">{children}</code>,
 
                 }}/>
-
     </div>
   </div>
 )

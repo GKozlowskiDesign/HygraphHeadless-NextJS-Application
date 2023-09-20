cfa5c7179e888cce28a0a06d9f297eee2ccad9c8
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-20 10:13:34 -0400

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
  
 import { getPosts, getPostDetails } from '../../services';
 
-import { PostDetail, Categories, PostWidget, Author, Loader } from '../../components'
+import { PostDetail, Categories, PostWidget, Loader } from '../../components'
 
 const PostDetails = ({ post }) => {
   const router = useRouter();
@@ -17,7 +17,6 @@ const PostDetails = ({ post }) => {
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
             <div className="col-span-1 lg:col-span-8">
                 <PostDetail post={post} />
-                <Author author={post.author} />
             </div>
             <div className="col-span-1 lg:col-span-4">
             <div className="relative lg:sticky top-8">

e8ea7c04dc6515d1b9222dc4f29b0ca19ad5b116
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-09-20 10:49:22 -0400

    Update About.jsx

--- components/Portfolio/About.jsx
+++ components/Portfolio/About.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
-
 const Biography = () => {
-
     return (
-<div className="
-    border-b-2 border-gray-200 shadow-sm">
-          <h3 className="text-xl text-black mb-2">Biography</h3>
-        <div className='grid grid-cols-1 mt-2 mb-10 text-left'>
-            <div className='text-sm p-7'><p>As a Business Information Systems Graduate, Full-Stack Developer, and 
-                AWS Certified Cloud Practitioner, I excel in navigating teams through technical 
-                challenges, cloud migration initiatives, workload architecture examinations, and 
-                high-risk issue mitigation. My proficiency bridges the gap between technical and 
-                business dimensions.
-                <br></br><br></br>
-                I actively engage in personal projects, courses, and certifications utilizing a 
-                spectrum of technologies that focus on development core of Full-Stack, FrontEnd, BackEnd, Cloud, Blockchain and Web3.
-                <br></br><br></br>
-                My GitHub repositories epitomize my dedication to continuous growth, perpetual learning, 
-                and professional advancement. They showcase hands-on experience and adeptness in 
-                constructing secure, reliable projects with cloud technologies.
-                </p>
+        <div className="
+            border-b-2 border-gray-200 shadow-sm">
+                  <h3 className="text-xl text-black mb-2">Biography</h3>
+                <div className='grid grid-cols-1 mt-2 mb-10 text-left'>
+                    <div className='text-sm p-7'><p>As a Business Information Systems Graduate, Full-Stack Developer, and 
+                        AWS Certified Cloud Practitioner, I excel in navigating teams through technical 
+                        challenges, cloud migration initiatives, workload architecture examinations, and 
+                        high-risk issue mitigation. My proficiency bridges the gap between technical and 
+                        business dimensions.
+                        <br></br><br></br>
+                        I actively engage in personal projects, courses, and certifications utilizing a 
+                        spectrum of technologies that focus on development core of Full-Stack, FrontEnd, BackEnd, Cloud, Blockchain and Web3.
+                        <br></br><br></br>
+                        My GitHub repositories epitomize my dedication to continuous growth, perpetual learning, 
+                        and professional advancement. They showcase hands-on experience and adeptness in 
+                        constructing secure, reliable projects with cloud technologies.
+                        </p>
+                    </div>
+                </div>  
             </div>
-        </div>  
-    </div>
-
-)
-}
-
+        )}
 export default Biography;

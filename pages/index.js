1bf90631329ddbce36ce4167f8e820de137fe0ef
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-10-12 14:17:11 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,10 @@ import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
 import Head from "next/head"
-
-
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
+    {/*<FeaturedPosts/>*/}
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (

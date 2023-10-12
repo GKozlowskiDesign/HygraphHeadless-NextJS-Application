8ddd1afe34f43050109e60e4ebef94e7bdc39240
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-10-12 14:09:09 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
-import  Portfolio  from '../components/Portfolio/Hero'
 import About  from '../components/Portfolio/About'
 import Head from "next/head"
 
@@ -14,19 +13,14 @@ export default function Home({ posts }) {
         <link rel="icon" href="/favicon.png" />
         <meta description="Checkout GKDesign the Portfolio Website of Gary Kozlowski As a Business Information Systems Graduate, Full-Stack Developer, and AWS Certified Cloud Practitioner." />
       </Head>
+    
       <div className="container mx-auto px-10 mb-8 ">
         {/*<FeaturedPosts/>*/}
           <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-            <div className="lg:col-span-8 col-span-1">
-              <Portfolio/>
-              {posts.map((post, index) => (
-                <PostCard key={index} post={post.node} />
-              ))}
-            </div>
             <div className="lg:col-span-4 col-span-1 lg:border-l-2 pl-6">
               <div className="lg:sticky relative top-8">
-              {/* Biography Paragraphs*/}
-              <div className='hidden lg:block md:hidden' alt="Biography Section"><About /></div>
+              {/* Biography Paragraphs
+              <div className='hidden lg:block md:hidden' alt="Biography Section"><About /></div> */}
               {/* List of Posts */}
               <div alt="Listed Posts"><PostWidget /></div>
               {/* List of Categories */}

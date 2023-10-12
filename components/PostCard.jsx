a1760f1ede9ff700347bf2b9892105084428508a
GKozlowskiDesign <82541715+GKozlowskiDesign@users.noreply.github.com>
2023-10-12 13:54:49 -0400

    Update PostCard.jsx

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,55 +1,53 @@
 import React from "react";
 import moment from 'moment';
 import Link from 'next/link';
-
 const PostCard = ({ post }) => {
     console.log(post);
   return (
-    <div className="bg-white border-b-2 border-gray-200 shadow-sm lg:p-4">
-         {/*    PostCard Row 1   */}
-        <div className="grid grid-cols-6">
-         {/*    Column One   */}     
-            <div className="flex justify-left mb-2 lg:mb-0 w-full lg:w-auto mr-4 col-span-2 mx-auto rounded-xl p-1">
-                <p className="inline text-center justify-center pt-1 md:pt-0 text-gray-700 text-xs">{post.author.name}</p>        
-            </div>  
-         {/*    Column Two       */}
-            <div className="font-medium items-right  justify-center  text-gray-700 col-span-4 mb-4 lg:mb-0 flex">
+    <div className="bg-white mt-2 border-y-1 border-gray-300 shadow-md shadow-gray-100 lg:p-0 rounded-lg">
+        <div className="grid grid-cols-6 pt-4 ">
+            {/*Column one*/}
+            <div className="justify-start col-span-3 p-2">
+             {/*    <p className="text-center justify-center pt-1 md:pt-0 text-gray-700 text-xs  md:text-sm">{post.author.name}</p>   */}
+            </div>
+            {/*Column two*/}
+            <div className="justify-center items-center  text-gray-700 col-span-3 mb-2 mt-1 lg:mb-0 flex text-sm p-2">
                 <span>
-                    {moment(post.createdAt).format('MMM DD, YYYY')}
+              {/*      {moment(post.createdAt).format('MMM DD, YYYY')} */}
                 </span>
             </div>
-    </div>
-        
-        {/*     PostCard Row 2    */}
-        <div className="grid grid-cols-1">   
-        {/*Column One*/}
-            <h1 className="transition duration-500 text-left mb-8 md:mt-2  mr-4 p-5 cursor-pointer text-black
+        </div>
+
+        <div className="grid-cols-1 grid">
+            <h1 className="transition duration-500 text-left mb-2 md:mt-2  mr-4 p-2 cursor-pointer text-black
             hover:text-indigo-500 text-xl">
                 <Link href={`/post/${post.slug}`}>
                     {post.title}
                 </Link>
             </h1>
         </div>
-
-        {/*     PostCard Row 3      */}
+        
+       
+                
         <div className="grid-cols-4 grid gap-6">
-        {/*Column One*/}
-            <div className="text-left items-center justify-center mb-2 w-full col-span-3">
+            {/*Column One*/}
+            <div className="p-8 text-left items-center justify-center mb-2 w-full col-span-3">
                 <Link href={`/post/${post.slug}`}>
                         <p className="text-sm text-black font-normal mx-auto mb-8">{post.excerpt}</p>
                 </Link>
             </div>
             
-        {/*      Column Two        */}
-            <div className="mb-2 rounded-md mx-auto">
+            {/*Column Two*/}
+            <div className="pt-8 rounded-md  mr-4 cols-span-3">
             <Link href={`/post/${post.slug}`}>
-                <img 
+                <img
                 src={post.featuredImage.url}
                 alt={post.title}
                 className="
                 w-full 
                 shadow-lg
-                rounded-md"
+                rounded-md justify-center items-center
+                "
                 />
             </Link>
             </div>
@@ -57,5 +55,7 @@ const PostCard = ({ post }) => {
     </div>
     )
 };
+
+export default PostCard;
 
 export default PostCard;
